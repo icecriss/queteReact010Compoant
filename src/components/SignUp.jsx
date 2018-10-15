cc1ebf348eb90b369748779a93303af5bfbc0e94
Chris Chatroux <icecriss@hotmail.com>
2018-10-15 15:02:55 +0200

    020 - Création d'un formulaire d'enregistrement
    files updated to solve the quest 020-Formulaire
    
    1- a form is created with 5 input:
    	email, password, password confirmation, firstname and last name
    
    2- instead of showing the email in h1, it's the component state that is displayed
    3- it is also displayed in the console.

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -4,15 +4,17 @@ class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: "address@email.co.uk",
+      email: "",
+      password: "",
+      passwordValid: false,
+      firstname: "",
+      lastname: "",
       focuss: ""
     };
   }
 
   changeEmail(e) {
-    // let h1Value = e.target.value.replace(/\*/i, "");
-    // e.target.value = h1Value;
-    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let reg = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
     if (reg.test(e.target.value)) {
       this.setState({ focuss: "test" });
     } else {
@@ -21,23 +23,121 @@ class SignUp extends Component {
     if (e.target.value.length !== 0) {
       this.setState({ email: e.target.value });
     } else {
-      this.setState({ email: "address@email.co.uk" });
+      this.setState({ email: "" });
     }
+  }
+
+  changePassword(e) {
+    if (e.target.value.length !== 0) {
+      this.setState({ password: e.target.value });
+      if (e.target.value !== document.getElementById("password2")) {
+        this.setState({
+          passwordValid: false
+        });
+      }
+    } else {
+      this.setState({
+        password: "",
+        passwordValid: false
+      });
+    }
+  }
+
+  checkPassword(e) {
+    if (e.target.value === this.state.password && this.state.password !== "") {
+      this.setState({ passwordValid: true });
+    } else {
+      this.setState({ passwordValid: false });
+    }
+  }
+
+  changeFirstname(e) {
+    if (e.target.value !== "") {
+      this.setState({ firstname: e.target.value });
+    } else {
+      this.setState({ firstname: "" });
+    }
+  }
+
+  changeLastname(e) {
+    if (e.target.value !== "") {
+      this.setState({ lastname: e.target.value });
+    } else {
+      this.setState({ lastname: "" });
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(JSON.stringify(this.state));
   }
 
   render() {
     return (
       <div>
-        <label htmlFor="email">
-          <h1>{this.state.email}</h1>
-        </label>
-        <input
-          className={this.state.focuss}
-          onChange={e => this.changeEmail(e)}
-          id="email"
-          name="email"
-          type="email"
-        />
+        <h1>
+          {JSON.stringify(this.state)
+            .split(",")
+            .join(", ")}
+        </h1>
+
+        <form>
+          <label htmlFor="email">Enter your email</label>
+          <input
+            className={this.state.focuss}
+            onChange={e => this.changeEmail(e)}
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Enter your email"
+            required
+          />
+
+          <label htmlFor="password1">Enter your password</label>
+          <input
+            onChange={e => this.changePassword(e)}
+            type="password"
+            id="password1"
+            name="password1"
+            placeholder="Enter your password"
+            required
+          />
+
+          <label htmlFor="password2">Enter your password again</label>
+          <input
+            onChange={e => this.checkPassword(e)}
+            type="password"
+            id="password2"
+            name="password2"
+            placeholder="Confirm your password"
+            required
+          />
+
+          <label htmlFor="firstname">Enter your firstname</label>
+          <input
+            onChange={e => this.changeFirstname(e)}
+            type="text"
+            name="firstname"
+            id="firstname"
+            placeholder="Enter your firstname"
+            required
+          />
+
+          <label htmlFor="lastname">Enter your lastname</label>
+          <input
+            onChange={e => this.changeLastname(e)}
+            type="text"
+            name="lastname"
+            id="lastname"
+            placeholder="Enter your lastname"
+            required
+          />
+          <input
+            onClick={e => this.handleSubmit(e)}
+            type="submit"
+            value="Soumettre"
+          />
+        </form>
       </div>
     );
   }
